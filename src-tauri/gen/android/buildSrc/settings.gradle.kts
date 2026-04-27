77e1843118937928195820f2f4815b89126b645e
丁乐 <dingle@jtkjbike.com>
2026-04-27 16:01:58 +0800

    add tauri

---
+++ src-tauri/gen/android/buildSrc/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/google")
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
